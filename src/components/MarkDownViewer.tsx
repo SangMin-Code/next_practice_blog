669242a8a6a1fb84c910b852c77d3fbeb71adeca
SangMin Lee <45862832+SangMin-Code@users.noreply.github.com>
2023-04-25 13:47:47 +0900

    markdown

---
+++ src/components/MarkDownViewer.tsx
@@ -0,0 +1,44 @@
+'use client';
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import remarkGfm from "remark-gfm";
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { materialDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import Image from "next/image";
+
+
+const MarkDownViewer =({content}:{content:string})=>{
+    return(
+        <ReactMarkdown 
+            className="prose lg:prose-xl max-w-none"
+            remarkPlugins={[remarkGfm]}
+             components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <SyntaxHighlighter
+              language={match[1]}
+              PreTag="div"
+              {...props}
+              style={materialDark}
+              >
+                {String(children).replace(/\n$/, "")}
+              </SyntaxHighlighter>
+            ) : (
+              <code {...props}>{children}</code>
+            );
+          },
+          img: (image) => (
+            <Image
+              src={image.src || ""}
+              alt={image.alt || ""}
+              width={500}
+              height={300}
+            //   className={classes.markdown_container_img}
+            />
+          ),
+        }}
+            >{content}</ReactMarkdown>
+    )
+}
+
+export default MarkDownViewer
