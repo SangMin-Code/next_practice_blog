2659a11c07a5b26c06aaa13737a8e90d1a043110
SangMin Lee <45862832+SangMin-Code@users.noreply.github.com>
2023-04-26 15:26:09 +0900

    adjacent post

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
+
 type Props = {
     categories:string[];
     selected:string;
-    onClick: (category:string)=>void;
+    // onClick: (category:string)=>void;
 }
 
-export default function Categories({categories,selected,onClick}:Props){
+export default function Categories({categories,selected}:Props){
+    
     return(
         <section className="text-center p-4">
             <h2 className="text-lg font-bold border-b border-sky-500 mb-2">Category</h2>
@@ -13,11 +16,11 @@ export default function Categories({categories,selected,onClick}:Props){
                 <li 
                     className={`cursor-pointer hover:text-sky-500 ${category===selected && 'text-sky-600'}`} 
                     key={category}
-                     onClick={()=>onClick(category)}>
+                    //  onClick={()=>onClick(category)}
+                     >
                     {category}
                 </li>)
                 }
-
             </ul>
         </section>
     )
