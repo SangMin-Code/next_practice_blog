80cd5ee4e6b52b9973e2cbea70766c00e0251d76
SangMin Lee <45862832+SangMin-Code@users.noreply.github.com>
2023-04-26 16:15:40 +0900

    about/ contact

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,26 @@
+import Hero from "@/components/Hero";
+
+const TITLE_CLASS = 'text-2xl font-bold text-gray-800 my-2'
+
 export default function About(){
     return(
-        <h1>About Page</h1>
+        <>
+            <Hero/>
+            <section className="bg-gray-100 shadow-lg p-8 m-8 text-center">
+                <h2 className={TITLE_CLASS }>Who Am I?</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br/>
+                Dolore doloribus numquam amet blanditiis excepturi facere quod vel, earum iusto. Assumenda vitae amet nihil aliquid beatae molestias enim dignissimos! Quisquam, quia.</p>
+                <h2 className={TITLE_CLASS }>Career</h2>
+                <p> test <br/>
+                    test <br/>
+                    test <br/>
+                </p>
+                <h2 className={TITLE_CLASS }>Skills</h2>
+                <p>React, Node<br/>
+                    Git<br/>
+                    VsCode
+                </p>
+            </section>
+        </>
     )
 }
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,5 +1,27 @@
+import ContactForm from "@/components/ContactForm"
+import { AiFillGithub, AiFillLinkedin, AiFillYoutube } from "react-icons/ai"
+
+const LINKS = [
+    {icon: <AiFillGithub/>, url:''},
+    {icon: <AiFillLinkedin/>, url:''},
+    {icon: <AiFillYoutube/>, url:''}
+]
+
 export default function Contact(){
     return(
-        <h1>Contact page</h1>
+        <section className="flex flex-col items-center">
+            <h2 className="text-3xl font-bold my-2">Contact me</h2>
+            <p>test@test.com</p>
+            <ul className="flex gap-4 my-2">
+                {LINKS.map((link,idx)=>
+                 <a key={idx} 
+                    href={link.url} 
+                    target="_blank" 
+                    rel="noreferrer" 
+                    className="text-5xl hover:text-red-400">{link.icon}</a>)}
+            </ul>
+            <h2 className="text-3xl font-bold my-8">Send me an email</h2>
+            <ContactForm/>
+        </section>
     )
 }
